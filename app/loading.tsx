70633f7aeb40d17d43b6a0f0d1e4daa953b31e0c
blancasanchezmarin0-beep <blancasanchezmarin0@gmail.com>
2026-05-21 12:05:05 +0200

    Rename Talego to Taleo and fix Next.js 15 Suspense issue by adding loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 bg-white z-[60] flex items-center justify-center">
+      <div className="font-playfair text-xl tracking-widest animate-pulse">
+        NAUT
+      </div>
+    </div>
+  );
+}
